d5f79acb249bf926cf11cbbbd7a647993890b486
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2023-01-28 17:15:53 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,4 +8,4 @@ def helloWorld():
     return "Hello World!"
 
 if __name__ == '__main__':
-    # app.run(host="127.0.0.1",port="8080", debug=True)
+    app.run(host="127.0.0.1",port="8080", debug=True)
