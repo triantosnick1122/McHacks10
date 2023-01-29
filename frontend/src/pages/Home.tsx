f10b03ab6735f1bfd5640b3d3279f78ee86116cc
Nicolas Almerge <nicolas.almerge@mail.mcgill.ca>
2023-01-29 11:20:14 +0000

    .

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,12 +15,12 @@ const MainForm: Component = () => {
     };
 
     return (
-        <Form onSubmit={handleSubmit} class="reddit-subreddit-search-bar">
+        <Form onSubmit={handleSubmit} class="reddit-subreddit-search-bar-form">
             <Row>
                 <Col>
-                    <InputGroup class="mb-3">
+                    <InputGroup>
                         <InputGroup.Text id="reddit-subredditlink-addon"><Image class="reddit-logo" src="/reddit-logo.png" alt="reddit.com"/>&nbsp;&nbsp;r/</InputGroup.Text>
-                        <Form.Control type="text" placeholder="Subreddit" value={redditSublink()} onInput={e => setRedditSublink(e.currentTarget.value)} aria-label="Subreddit" aria-describedby="reddit-subredditlink-addon"/>
+                        <Form.Control type="text" placeholder="Search a subreddit" value={redditSublink()} onInput={e => setRedditSublink(e.currentTarget.value)} aria-label="Subreddit" aria-describedby="reddit-subredditlink-addon"/>
                         <Button type="submit" disabled={redditSublink() === ""}><SearchIcon/></Button>
                     </InputGroup>
                 </Col>
@@ -29,15 +29,26 @@ const MainForm: Component = () => {
     )
 }
 
+const MainPage: Component = () => {
+    return (
+        <div class="home-main-element">
+            <div>
+            <TypingText text="Want to know more about a subreddit?" startWithLine={true} startAfter={0} finishAfter={2}/>
+            <TypingText text="We got everything you need!" startWithLine={true} startAfter={2} finishAfter={-1}/>
+            <br/><br/><br/><br/>
+            <MainForm/>
+            <br/><br/>
+            </div>
+        </div>
+    );
+}
+
 const HomePage: Component = () => {
     return (
         <>
             <NavMenu/>
-            <br/>
-            <MainForm/>
-            <br/>
-            <TypingText text="Want to know more about a subreddit?" startWithLine={true} startAfter={0} finishAfter={3}/>
-            <TypingText text="We got everything you need!" startWithLine={false} startAfter={3} finishAfter={-1}/>
+            <MainPage/>
+
             <div class="center">
                 <CircleBar title="Toxicity" value={80}/>
                 <CircleBar title="Misinformation" value={23}/>
