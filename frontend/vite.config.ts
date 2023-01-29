1c8b242b5bd41fd818823f6485f530b5535e2f5b
Nicolas Almerge <nicolas.almerge@mail.mcgill.ca>
2023-01-29 08:35:06 +0000

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+    assetsInclude: ["src/assets/*.*"],
     plugins: [solidPlugin()],
     server: {
         port: 3000,
     },
     build: {
-        target: 'esnext',
+        target: "esnext",
     },
 });
