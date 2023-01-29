6c779ca00bacd7e5672fd980eed39e2bfec026f9
Nicolas Almerge <nicolas.almerge@mail.mcgill.ca>
2023-01-29 07:15:11 +0000

    .

--- frontend/src/components/CircleBar.tsx
+++ frontend/src/components/CircleBar.tsx
@@ -1,17 +1,20 @@
 import { Component, createEffect, createSignal } from "solid-js";
 import { CircularProgress, CircularProgressIndicator, CircularProgressLabel } from "@hope-ui/solid";
+import "./styles/CircleBar.scss";
 
 const CircleBar: Component<{value: number}> = (props) => {
     const [currValue, setCurrValue] = createSignal(0);
-
-    let interval = setInterval(() => setCurrValue(currValue() + 9 > props.value? props.value: currValue() + 9), 50);
+    let interval = setInterval(() => setCurrValue(currValue() + 1), 25);
     createEffect(() => {if (currValue() >= props.value) clearInterval(interval)});
 
     return (
-        <CircularProgress value={currValue()} size="120px">
-            <CircularProgressIndicator withRoundCaps color={currValue() <= 33? "$success10": currValue() <= 66? "$warning10": "$danger10"}/>
-            <CircularProgressLabel fontSize="30px"/>
-        </CircularProgress>
+        <div class="center col-4">
+            <p class="circle-bar-title">Toxicity</p>
+            <CircularProgress value={currValue()} size="120px">
+                <CircularProgressIndicator withRoundCaps color={currValue() <= 33? "$success10": currValue() <= 66? "$warning10": "$danger10"}/>
+                <CircularProgressLabel fontSize="30px"/>
+            </CircularProgress>
+        </div>
     );
 }
 
